35e8a1f624e8dd637d42260b7eaa93f80a069851
Pablo Quagliata <pablo.quagliata@gmail.com>
2021-10-17 21:25:39 -0300

    Work in progress

--- resume/js/modal.js
+++ resume/js/modal.js
@@ -1,21 +1,58 @@
 const modal = document.getElementById("my-modal");
-const modalButtons = document.querySelectorAll('.modal-open')
+const modalTitle = document.getElementById("job-title");
+const modalParagraph = document.getElementById("job-paragraph");
 const okButton = document.getElementById("ok-btn");
 
-const toggleModal = () => modal.style.display = "block";
-
-// open modal
-for (var i = 0; i < modalButtons.length; i++) {
-    modalButtons[i].addEventListener('click', toggleModal)
+const showModal = (button) => {
+    switch (button) {
+        case "meli":
+            modalTitle.innerHTML = "Mercado Libre"
+            modalParagraph.innerHTML = `
+            Mostly working with iOS mobile apps using Swift and Objective-C refactoring. <br>
+            Solid understanding of object-oriented programming. <br>
+            Knowledge of memory management and multi-threading. <br>
+            Good sense of UI design and user-oriented focus, with an understanding of Apple’s design principles and interface guidelines. <br>
+            Experience with code versioning tools (Git).<br>
+            `
+            break;
+        case "orange":
+            modalTitle.innerHTML = "OrangeLoops"
+            modalParagraph.innerHTML = `
+            Worked with custom styling in white-label Objective-C apps and Unit and UI testing using Xcode. <br>
+            Usage of versioning tools.
+            `
+            break;
+        case "thinkup":
+            modalTitle.innerHTML = "ThinkUp Software"
+            modalParagraph.innerHTML = `
+            Mostly working with iOS mobile apps using Swift and Objective-C refactoring. <br>
+            Solid understanding of object-oriented programming. <br>
+            Knowledge of memory management and multi-threading. <br>
+            Good sense of UI design and user-oriented focus, with an understanding of Apple’s design principles and interface guidelines. <br>
+            Experience with code versioning tools (Git).<br>
+            `
+            break;
+        case "kpmg":
+            modalTitle.innerHTML = "KPMG"
+            modalParagraph.innerHTML = `
+            Worked with RPA automation tools such as Automation Anywhere, UI Path and WorkFusion development tools, for projects meant for Uruguayan technological and accounting companies. <br> 
+            Functional Testing and helped in the design of Javascript websites. <br>
+            Data Analytics using IDEA Software and Python developed scripts.
+            `
+            break;
+        default:
+            break;
+        
+    }
+    // display modal
+    modal.style.display = "block"
 }
 
 // close modal
-okButton.onclick = function () {
-    modal.style.display = "none";
-}
+okButton.onclick = () => modal.style.display = "none";
 
 // register clicks outside modal
-window.onclick = function (event) {
+window.onclick = (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
     }
