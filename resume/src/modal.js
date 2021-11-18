c6ac0c80435f67b6eb19b7b6689e863ad2e34782
Pablo Quagliata <pablo.quagliata@gmail.com>
2021-11-17 21:23:11 -0300

    WIP with POST fetch. Added browser-sync for auto refresh.

--- resume/src/modal.js
+++ resume/src/modal.js
@@ -6,6 +6,9 @@ var jobModalStartDate = document.getElementById("site-start-date");
 var jobModalEndDate = document.getElementById("site-end-date");
 var jobModalEndDateContainer = document.getElementById("end-date-container");
 var contactModal = document.getElementById("contact-modal");
+var contactName = (document.getElementById("contact-full-name"));
+var contactEmail = (document.getElementById("contact-email"));
+var contactMessage = (document.getElementById("contact-message"));
 var hireMeButton = document.getElementById("hire-me-btn");
 var okButton = document.getElementById("ok-btn");
 var Modals;
@@ -37,11 +40,49 @@ var showJobModal = function (button) {
     });
     jobModal.style.display = "block";
 };
+var form = document.getElementById('form');
+var handleSubmit = function (event) {
+    event.preventDefault();
+    var data = {
+        name: contactName.value,
+        email: contactEmail.value,
+    };
+    fetch("https://PW2021-APINode-pabloq1.pabloq1.repl.co", {
+        method: "POST",
+        credentials: 'include',
+        headers: {
+            "Content-Type": "text/plain",
+        },
+        body: JSON.stringify({ data: data }),
+    })
+        .then(function (res) { return res.json(); })
+        .then(function (data) {
+        closeModal(Modals.Contact);
+        console.log(data);
+    })
+        .catch(function (error) {
+        alert("" + error);
+        console.log(error);
+    });
+};
+form.addEventListener('submit', handleSubmit);
+var showContactModal = function () { return (contactModal.style.display = "block"); };
+window.onclick = function (event) {
+    switch (event.target) {
+        case jobModal:
+            closeModal(Modals.Job);
+            break;
+        case contactModal:
+            closeModal(Modals.Contact);
+            break;
+        default:
+            break;
+    }
+};
 var closeModal = function (id) {
     switch (id) {
         case Modals.Contact:
             contactModal.style.display = "none";
-            sentMessageAlert();
             break;
         case Modals.Job:
             jobModal.style.display = "none";
@@ -50,17 +91,3 @@ var closeModal = function (id) {
             break;
     }
 };
-var showContactModal = function () { return (contactModal.style.display = "block"); };
-window.onclick = function (event) {
-    switch (event.target) {
-        case jobModal:
-            jobModal.style.display = "none";
-            break;
-        case contactModal:
-            contactModal.style.display = "none";
-            break;
-        default:
-            break;
-    }
-};
-var sentMessageAlert = function () { return alert("Thank you!"); };
--- resume/src/modal.ts
+++ resume/src/modal.ts
@@ -1,3 +1,4 @@
+// job modal
 const jobModal: HTMLElement = document.getElementById("job-modal");
 const jobModalTitle: HTMLElement = document.getElementById("site-title");
 const jobModalPosition: HTMLElement = document.getElementById("site-position");
@@ -9,8 +10,19 @@ const jobModalEndDate: HTMLElement = document.getElementById("site-end-date");
 const jobModalEndDateContainer: HTMLElement =
     document.getElementById("end-date-container");
 
+// contact modal
 const contactModal: HTMLElement = document.getElementById("contact-modal");
+const contactName: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById("contact-full-name")
+);
+const contactEmail: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById("contact-email")
+);
+const contactMessage: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById("contact-message")
+);
 
+// buttons
 const hireMeButton: HTMLElement = document.getElementById("hire-me-btn");
 const okButton: HTMLElement = document.getElementById("ok-btn");
 
@@ -25,12 +37,11 @@ const showJobModal = (button: string) => {
             return response.json();
         })
         .then((data) => {
-            
             if (data.endDate == "") {
-                jobModalEndDateContainer.style.display = "none"
+                jobModalEndDateContainer.style.display = "none";
             } else {
-                jobModalEndDateContainer.style.display = "grid"
-                jobModalEndDate.innerHTML = `${data.endDate}`
+                jobModalEndDateContainer.style.display = "grid";
+                jobModalEndDate.innerHTML = `${data.endDate}`;
             }
 
             jobModalTitle.innerHTML = data.title;
@@ -46,11 +57,56 @@ const showJobModal = (button: string) => {
     jobModal.style.display = "block";
 };
 
+const form = document.getElementById('form');
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let data = {
+        name: contactName.value,
+        email: contactEmail.value,
+    };
+
+    fetch("https://PW2021-APINode-pabloq1.pabloq1.repl.co", {
+        method: "POST",
+        credentials: 'include',
+        headers: {
+            "Content-Type": "text/plain",
+        },
+        body: JSON.stringify({ data }),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            closeModal(Modals.Contact)
+            console.log(data);
+        })
+        .catch((error) => {
+            alert(`${error}`);
+            console.log(error);
+        });
+}
+
+form.addEventListener('submit', handleSubmit)
+
+const showContactModal = () => (contactModal.style.display = "block");
+
+window.onclick = (event: Event) => {
+    switch (event.target) {
+        case jobModal:
+            closeModal(Modals.Job)
+            break;
+        case contactModal:
+            closeModal(Modals.Contact)
+            break;
+        default:
+            break;
+    }
+};
+
 const closeModal = (id: string) => {
     switch (id) {
         case Modals.Contact:
             contactModal.style.display = "none";
-            sentMessageAlert();
             break;
         case Modals.Job:
             jobModal.style.display = "none";
@@ -59,20 +115,3 @@ const closeModal = (id: string) => {
             break;
     }
 };
-
-const showContactModal = () => (contactModal.style.display = "block");
-
-window.onclick = (event: Event) => {
-    switch (event.target) {
-        case jobModal:
-            jobModal.style.display = "none";
-            break;
-        case contactModal:
-            contactModal.style.display = "none";
-            break;
-        default:
-            break;
-    }
-};
-
-const sentMessageAlert = () => alert("Thank you!");
